6afe86ac7e2720231a0d75fb20e4498d18c0b146
Alex <Sunny.hsia1785@gmail.com>
2020-06-13 15:25:46 +0800

    update traffic-accident detector

--- detectors/traffic-accident/detect_traffic.py
+++ detectors/traffic-accident/detect_traffic.py
@@ -3,4 +3,18 @@ import efficientnet.keras
 import efficientnet.tfkeras
 from PIL import Image
 import numpy as np
+import argparse
+import os
 
+cwd = os.getcwd()
+print(cwd + "/traffic-net10.h5")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("image_path", help="Input path to image you want to predict")
+args = parser.parse_args()
+img = Image.open(args.image_path)
+new_img = np.array(img.resize((224,224)))
+new_img = np.expand_dims(new_img, axis=0)
+
+model = tf.keras.models.load_model(cwd + "/traffic-net10.h5")
+print("Accident Probability:", 1- model.predict(new_img))
