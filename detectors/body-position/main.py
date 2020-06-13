b1e6e67cbe0a775ef31ccf53a450430eb34db800
Alex <Sunny.hsia1785@gmail.com>
2020-06-13 22:06:54 +0800

    turn body-detector into API

--- detectors/body-position/main.py
+++ detectors/body-position/main.py
@@ -1,8 +1,13 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import requests
 import sys
 import math
+import io
 
 POSEENDPOINT = "https://scdfxibm2020.garykim.dev/pose/model/predict"
+
+HOSTNAME = "0.0.0.0"
+PORT = 8080
 
 # Top down body parts
 PRIORITIES = ["REye", "LEye", "REar", "LEar", "Nose", "Neck", "RShoudler", "LShoulder", "RHip", "LHip", "RKnee", "LKnee", "LAnkle", "RAnkle"]
@@ -49,8 +54,34 @@ def significantPoint(top=False, body_parts=[]):
         'y': 100 if top else 0
     }
 
+
+class Server(BaseHTTPRequestHandler):
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        length = int(self.headers['content-length'])
+        thisline = self.rfile.readline() + self.rfile.readline() + self.rfile.readline() + self.rfile.readline()
+        length -= len(thisline)
+        data = self.rfile.read(length)
+
+        self.wfile.write(bytes(str(1 - isLyingDown(io.BytesIO(data))), 'utf8'))
+        self.send_response(200)
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 1:
         print("Expecting 1 command line argument")
         sys.exit(1)
-    print(isLyingDown(sys.argv[1]))
+    webServer = HTTPServer((HOSTNAME, PORT), Server)
+
+    try:
+        webServer.serve_forever()
+        print("started")
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
+
+
